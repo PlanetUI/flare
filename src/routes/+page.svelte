29152b27a2ef8f54b98f2cc7f54facf015cd4d04
Mashanz <mangatkk@gmail.com>
2023-01-10 15:24:41 +0700

    auto commit and push command

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class=" text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
