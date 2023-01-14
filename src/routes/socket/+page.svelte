34f849ec0d8a6fecfe9867cfe57a614ed3b3755a
Mashanz <mangatkk@gmail.com>
2023-01-14 07:41:43 +0700

    chore: move socket page test

---
+++ src/routes/socket/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { io } from 'socket.io-client';
+
+	const socket = io();
+
+	socket.on('eventFromServer', (message) => {
+		console.log(message);
+	});
+</script>
+
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class=" text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
