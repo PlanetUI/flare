1e3b63f1ebbc71fd4fafaff7bbf2eba1397a234e
Mashanz <mangatkk@gmail.com>
2023-01-14 10:57:50 +0700

    feat: example get data

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-    return new Response();
+export const GET: RequestHandler = async (event) => {
+    return json(event);
 };
