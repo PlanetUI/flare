e5d6602182e5c3e53fa8a5a0c5a66d1cbeed344c
Mashanz <mangatkk@gmail.com>
2023-01-18 16:53:30 +0700

    fix: login

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,10 +1,24 @@
+<script>
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+</script>
+
 <header
 	class="bg-black-2 text-text-1 font-overpass top-0 z-[100] fixed flex w-full h-16 px-4 shadow">
 	<div class="m-auto flex w-full">
-		<h1 class="flex-grow font-semibold">PRACTICAL <span class="text-thame-1">SVELTEKIT</span></h1>
+		<a href="/" class="flex-grow font-semibold">
+			PRACTICAL <span class="text-thame-1">SVELTEKIT</span>
+		</a>
 		<nav class="flex gap-4">
-			<a href="/">Home</a>
-			<a href="/auth">Login</a>
+			{#if $page.data.session}
+				<a href="/dash">Dashboard</a>
+				<em>
+					Hi 👋🏻 <strong>{$page.data.session.user?.name ?? 'User'}</strong>
+				</em>
+				<button on:click={() => signOut()}>🚪Logout</button>
+			{:else}
+				<button on:click={() => signIn('google')}>Sign in with Google</button>
+			{/if}
 		</nav>
 	</div>
 </header>
