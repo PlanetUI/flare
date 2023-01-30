00640c0075c8ec1c44c69b1cc4b02ba0fe2e480b
iamdewaa <dewasembiring1@gmail.com>
2023-01-30 16:20:53 +0700

    Update register user

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,13 +1,17 @@
 import { MongoClient } from 'mongodb';
-import { env } from '$env/dynamic/private';
-
-//declare connection and db name
-const url = 'mongodb://localhost:27017';
-const dbname = 'flare';
+import { DATABASE_URL } from '$env/static/private';
 
 export async function db_insert_user(collection_name: string, document: Document) {
-	const data = {};
+	const client = new MongoClient(DATABASE_URL);
+	await client.connect();
+	const db = client.db(`${import.meta.env.MODE}_flare`);
+	const doc = db.collection(collection_name);
+	const data = await doc.insertOne(document);
+	client.close();
+
 	return data;
 }
 
-console.log(env.DATABASE_URL);
+export async function db_get_user(collection_name: string, document: Document) {
+	return;
+}
