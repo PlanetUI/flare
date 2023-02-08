d77724653135fac97c00309cdf082b8701af2955
Mashanz <mangatkk@gmail.com>
2023-02-08 08:16:03 +0700

    fix: saving users

--- src/routes/(dash)/dash/+layout.server.ts
+++ src/routes/(dash)/dash/+layout.server.ts
@@ -1,3 +1,4 @@
+import { db_get_user } from '$lib/server/db';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
@@ -5,6 +6,8 @@ export const load = (async (event) => {
 	const session = await event.locals.getSession();
 	if (!session) {
 		throw redirect(307, '/auth');
+	} else {
+		await db_get_user({ name: session?.user?.name, email: session.user?.email });
 	}
 	return {};
 }) satisfies LayoutServerLoad;
