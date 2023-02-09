13047c2ba12c21f4fd590ab6290073c6ffa03eb7
Mashanz <mangatkk@gmail.com>
2023-02-09 09:39:30 +0700

    feat: svx support import components

--- src/lib/components/filename.svelte
+++ src/lib/components/filename.svelte
@@ -1,9 +1,10 @@
 <script>
-	export let logo = 'https://svelte.dev/logo.png';
+	export let logo = 'https://svelte.dev/favicon.png';
 	export let name = 'name.js';
 </script>
 
-<figure>
-	<img src={logo} alt={name} />
-	<figcaption>{name}</figcaption>
+<figure
+	class="flex bg-black max-w-max border-t border-x border-thame-1-variant outline-thame-1 p-2">
+	<img class="w-8 h-8" src={logo} alt={name} />
+	<figcaption class="my-auto text-text-3 font-mono">{name}</figcaption>
 </figure>
