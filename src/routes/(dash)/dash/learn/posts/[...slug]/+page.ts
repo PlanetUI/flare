3c4a7b6c8ff106530293770718132f38b9bebe85
Mashanz <mangatkk@gmail.com>
2023-02-15 17:56:23 +0700

    fix: styling and theme

--- src/routes/(dash)/dash/learn/posts/[...slug]/+page.ts
+++ src/routes/(dash)/dash/learn/posts/[...slug]/+page.ts
@@ -6,8 +6,6 @@ export const load: PageLoad = async ({ params }) => {
 	const modules = import.meta.glob(`/src/posts/**/*.{md,svx,svelte.md}`);
 	let match: { path?: string; resolver?: App.MdsvexResolver } = {};
 	for (const [path, resolver] of Object.entries(modules)) {
-		console.log('paths', path);
-		console.log('slugFromPath', slugFromPath(path));
 		if (slugFromPath(path) === params.slug) {
 			match = { path, resolver: resolver as unknown as App.MdsvexResolver };
 			const post = await match.resolver?.();
