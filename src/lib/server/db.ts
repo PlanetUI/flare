ba07d211728f8ed024ab51c4c36f196699874d76
Mashanz <mangatkk@gmail.com>
2023-02-16 12:47:15 +0700

    feat: user counts

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -21,3 +21,37 @@ export async function db_get_user(document: Document) {
 	// return user data
 	return data;
 }
+
+export async function db_count_users() {
+	// connect to db
+	const client = new MongoClient(env.DATABASE_URL || '');
+	await client.connect();
+
+	// get db and collection
+	const db = client.db(`${import.meta.env.MODE}_flare`);
+	const doc = db.collection('users');
+
+	// get user from db
+	const data = await doc.countDocuments();
+	client.close();
+
+	// return user data
+	return data;
+}
+
+export async function db_count_premium_users() {
+	// connect to db
+	const client = new MongoClient(env.DATABASE_URL || '');
+	await client.connect();
+
+	// get db and collection
+	const db = client.db(`${import.meta.env.MODE}_flare`);
+	const doc = db.collection('users');
+
+	// get user from db
+	const data = await doc.countDocuments({ is_premium_user: true });
+	client.close();
+
+	// return user data
+	return data;
+}
---
+++ src/routes/(app)/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+import { db_count_premium_users, db_count_users } from '$lib/server/db';
+
+export const load: PageServerLoad = async (event) => {
+    return {
+        users: await db_count_users(),
+        premium_users: await db_count_premium_users(),
+    }
+}
