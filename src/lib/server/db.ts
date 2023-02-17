7c680171a1b4fc7b7fa6028ce7b1ce00c4258eb0
Mashanz <mangatkk@gmail.com>
2023-02-18 05:57:46 +0700

    fix: created at users

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -11,19 +11,27 @@ export async function db_get_user(document: Document) {
 	const db = client.db(`${import.meta.env.MODE}_flare`);
 	const doc = db.collection('users');
 
-	// check if user exists, if not, create/update it
 	const now = Date.now()
 	const now_iso = new Date(now).toISOString()
 	const now_formatted = new Date(now).toLocaleString('id', { timeZone: 'Asia/jakarta' })
-	const new_document = {
-		...document,
-		last_active_iso: now_iso,
-		last_active_formatted: now_formatted
-	}
-	await doc.updateOne({ email: document.email }, { $set: new_document }, { upsert: true });
 
 	// get user from db
 	const data = await doc.findOne({ email: document.email });
+	if (!data) {
+		// create user
+		const new_document_create = {
+			...document,
+			created_at: now_iso,
+		}
+		await doc.insertOne(new_document_create);
+	} else {
+		const new_document_update = {
+			...document,
+			last_active_iso: now_iso,
+			last_active_formatted: now_formatted
+		}
+		await doc.updateOne({ email: document.email }, { $set: new_document_update }, { upsert: true });
+	}
 	client.close();
 
 	// return user data
