64d4b3c4df3585700de2a578889e47f16d58abe6
Mashanz <mangatkk@gmail.com>
2023-02-20 13:03:27 +0700

    fix: notifications for early access

--- src/routes/(dash)/dash/learn/+page.svelte
+++ src/routes/(dash)/dash/learn/+page.svelte
@@ -5,12 +5,16 @@
 	const handleError = (ev: any) => (ev.target.src = '/favicon.png');
 </script>
 
-<main class="w-full md:mx-4">
+<main class="w-full md:px-4 bg-black-1 h-max min-h-max">
+	<div
+		class="border bg-black-7 font-bold p-4 mt-4 text-green-500 rounded border-green-500 border-l-8 mx-4 md:mx-0">
+		Akses awal, akan ada konten yang kurang dan beberapa fitur belum tersedia.
+	</div>
 	<div class="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 md:gap-2 gap-0.5 mx-auto md:py-8">
 		{#each data.posts as { slug, title, author, description, updated_at, thumbnail, is_premium }}
 			<a
 				href={'/dash/learn/posts/' + slug}
-				class=" h-min md:rounded-lg hover:outline-thame-1-variant hover:outline hover:outline-2 hover:outline-offset-2 hover:z-10">
+				class=" h-min md:rounded-lg md:border md:border-thame-1-variant md:border-opacity-50 hover:outline-thame-1-variant hover:outline hover:outline-2 hover:z-10">
 				<div class="flex h-64">
 					<img
 						class="m-auto w-40"
--- src/routes/(dash)/dash/roadmaps/+layout.svelte
+++ src/routes/(dash)/dash/roadmaps/+layout.svelte
@@ -1,3 +1,6 @@
 <main class="w-full bg-black-1 p-4 h-max">
+	<div class="border bg-black-7 font-bold p-4 text-green-500 rounded border-green-500 border-l-8">
+		Akses awal, akan ada konten yang kurang dan beberapa fitur belum tersedia.
+	</div>
 	<slot />
 </main>
