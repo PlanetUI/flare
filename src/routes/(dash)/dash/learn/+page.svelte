00907005c094098830da326af67062d5bb220fdc
Mashanz <mangatkk@gmail.com>
2023-02-22 10:55:19 +0700

    fix: styling premium and free badge

--- src/routes/(dash)/dash/learn/+page.svelte
+++ src/routes/(dash)/dash/learn/+page.svelte
@@ -8,14 +8,26 @@
 <main class="w-full md:px-4 bg-black-1 h-max min-h-max">
 	<div
 		class="border bg-black-7 font-bold p-4 mt-4 text-green-500 rounded border-green-500 border-l-8 mx-4 md:mx-0">
-		Akses awal, akan ada konten yang kurang dan beberapa fitur belum tersedia.
+		Akses awal, akan ada konten dan fitur belum tersedia.
 	</div>
-	<div class="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 md:gap-2 gap-0.5 mx-auto md:py-8">
+	<div
+		class="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-2 gap-0.5 mx-auto md:py-8">
 		{#each data.posts as { slug, title, author, description, updated_at, thumbnail, is_premium }}
 			<a
 				data-sveltekit-reload
 				href={'/dash/learn/posts/' + slug}
-				class=" h-min md:rounded-lg md:border md:border-thame-1-variant md:border-opacity-50 hover:outline-thame-1-variant hover:outline hover:outline-2 hover:z-10">
+				class="relative h-min md:rounded-lg md:border md:border-thame-1-variant md:border-opacity-50 hover:outline-thame-1-variant hover:outline hover:outline-2 hover:z-10">
+				<div class="absolute right-4 p-0">
+					<span
+						class:text-green-500={!is_premium}
+						class:border-green-500={!is_premium}
+						class:text-thame-1={is_premium}
+						class:border-thame-1={is_premium}
+						class="text-xs border font-mono  rounded px-1">
+						{is_premium ? 'PREMIUM' : 'FREE'}
+					</span>
+				</div>
+
 				<div class="flex h-64">
 					<img
 						class="m-auto w-40"
@@ -23,18 +35,10 @@
 						on:error={handleError}
 						alt={slug} />
 				</div>
-				<div class="p-4">
+				<div class="px-4">
 					<h5 class="text-thame-1">{title}</h5>
 					<div class=" text-thame-3 text-sm font-mono">{author} {updated_at}</div>
 					<p>{description}</p>
-				</div>
-				<div class="p-4">
-					<div
-						class:bg-thame-1-variant={is_premium}
-						class:bg-thame-3-variant={!is_premium}
-						class="text-white px-4 py-2 rounded text-center">
-						{is_premium ? 'Premium' : 'Gratis'}
-					</div>
 				</div>
 			</a>
 		{/each}
